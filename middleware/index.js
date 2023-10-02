cc9aa86e785804a871771483be3bc0c8c28ebe15
Walter <walterdcarrizo.19@gmail.com>
2023-10-02 11:38:29 -0300

    Se crearon controller, rutas, esquemas para Doctor

--- middleware/index.js
+++ middleware/index.js
@@ -1,6 +1,3 @@
-// import { validateFields } from '../middleware/validateFields.js';
-// import { validateJWT } from '../middleware/validateJTW.js';
-// import { isAdminRole } from '../middleware/validateRoles.js';
 
 export * from './jwtValidation.js';
 export * from './validateFields.js';
--- model/category.schema.js
+++ model/category.schema.js
@@ -1,10 +1,23 @@
-import { Schema } from 'mongoose';
+import mongoose,{ Schema, model } from 'mongoose';
 
-const categorySchema = new Schema({
-    name: string,
-    active: Boolean,
-    require: true
-    //Relacion con los doctores
+const CategorySchema = new Schema({
+
+    category: {
+        type: String,
+        required: [true, 'El rol es obligatorio']
+    },
+    active: {
+        type: Boolean,
+        default: true,
+        required: true
+    },
+    doctors: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Doctor',
+    }
+
 });
 
-export const categoryModel = mongoose.model('user', categorySchema);
+const CategoryModel = model('Category', CategorySchema);
+
+export default CategoryModel;
