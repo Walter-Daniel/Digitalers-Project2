481d65f886233d2d572e4d93820b4e885afe8528
Walter <walterdcarrizo.19@gmail.com>
2023-10-05 06:49:19 -0300

    Si no hay token, se redirecciona al login. Limpieza de código

--- src/middleware/jwtHeader.js
+++ src/middleware/jwtHeader.js
@@ -2,6 +2,9 @@ import { response } from "express";
 
 export const tokenInHeader = ((req, res=response, next) => {
     const token = req.cookies.token;
+    if(!token){
+      res.redirect('/auth/login')
+    }
     req.headers['x-token'] = token;
     next();
   });
