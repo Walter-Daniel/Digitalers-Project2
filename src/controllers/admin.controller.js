3387b4f34d9d1ea9a45032aa770d6d2d28ddd6d2
Walter <walterdcarrizo.19@gmail.com>
2023-11-08 07:48:10 -0300

    Se agregó efecto fade-in en home y administracion

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -1,11 +1,24 @@
 import { response } from "express";
+import User from '../model/user.schema.js';
+import Doctor from '../model/doctor.schema.js';
+import Appointment from '../model/appointment.schema.js';
 
-export const renderAdminPage = (req, res =response) => {
+export const renderAdminPage = async(req, res =response) => {
+
+    const [ appointments, doctors, users ] = await Promise.all([
+        Appointment.find({}).countDocuments(),
+        Doctor.find({}).countDocuments(),
+        User.find({}).countDocuments()
+    ]);
+
 
     res.render('admin/dashboard', {
         pageName: 'Dashboard',
         user: req.user,
         navbar: true,
-        footer: true
+        footer: true,
+        appointments,
+        users,
+        doctors
     })
 }
